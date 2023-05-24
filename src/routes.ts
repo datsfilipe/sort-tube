067489904eeb745a5d307a3732576df7243a7a6b
datsfilipe <datsfilipe.foss@proton.me>
2023-05-24 08:45:20 -0300

    feat: add routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,28 @@
 import { Router } from "express";
+import { RandomTweetController } from "./controllers/randomTweetController";
 
 const router = Router();
+
+const routes = {
+  "/": {
+    get: {
+      description: "Show the API status and endpoints.",
+    },
+  },
+  "/random-tweet": {
+    get: {
+      description: "Show a random tweet.",
+    },
+  },
+};
+
+router.get("/", (_, response) => {
+  return response.json({
+    message: "Welcome to the Random Tweet API!",
+    routes,
+  });
+});
+  
+router.get("/random-tweet", new RandomTweetController().handle);
 
 export default router;
